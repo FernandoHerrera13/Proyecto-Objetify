7718b642c10cff03de75fc265758a26b1ecffcff
Fernando Herrera <poko_fernando@hotmail.com>
2017-08-31 11:12:14 -0500

    Ya muestra reproducir

---
+++ Biblioteca.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Biblioteca
+{
+    private ArrayList<String> canciones;
+    private Reproductor reproductor;
+    
+    public Biblioteca()
+    {
+        canciones= new ArrayList<String>();
+        reproductor= new Reproductor();
+    }
+    
+    public void agregarCancion(String nombreArchivo)
+    {
+        canciones.add(nombreArchivo);
+    }
+    
+    public void reproducir(int numCancion)
+    {
+        reproductor.reproducir( canciones.get(numCancion) );
+    }
+}
---
+++ Reproductor.java
@@ -0,0 +1,8 @@
+public class Reproductor
+{
+    public void reproducir(String nombreArchivo)
+    {
+        //Simulacion de la reproduccion
+        System.out.println("Reproduciendo: "+nombreArchivo);
+    }
+}
